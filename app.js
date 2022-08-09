56dddac7e343aee9f62cd63908ba24568dda086f
baldgenie <siddhartha.kapoor@gmail.com>
2022-08-09 13:04:12 +0545

    commit

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const mongoose = require("mongoose");
 const express = require("express");
+const app = express();
+
 const cors = require("cors");
 const passport = require("passport");
 const bcrypt = require("bcryptjs");
 const bodyParser = require("body-parser");
-const app = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 var express_Session = require('express-session')
 const jwt = require('jsonwebtoken');
 const RefreshTokenSchema = require('./models/refreshTokensDB')
@@ -32,7 +34,6 @@ mongoose.connect(
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("*", cors());
-app.use(cors());
 
 
 
